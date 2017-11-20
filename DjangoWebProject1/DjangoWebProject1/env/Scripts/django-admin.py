33116a801a8b35a7ecbd63c6f5c2d28a312eb2a6
stshibayama <shibayama@stailor.co.jp>
2017-11-20 21:32:01 +0900

    ok

---
+++ DjangoWebProject1/DjangoWebProject1/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\Users\siva\source\repos\djangoforms\gitpractce\DjangoWebProject1\DjangoWebProject1\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
